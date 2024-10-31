bfa4808215df1eb213312710440d265b8005a800
Luan Pham <luan4637@gmail.com>
2024-10-31 09:57:43 +0700

    change port

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,2 +1,2 @@
-export const IMAGE_URL = 'http://phpblog.private';
-export const API_URL = 'http://phpblog.private';
+export const IMAGE_URL = 'http://phpblog.private:8080';
+export const API_URL = 'http://phpblog.private:8080';
